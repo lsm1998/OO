0172f76b06f7df0ca620ae549874e25fb4df4d3c
lsm <487005831@qq.com>
2021-05-17 22:12:16 +0800

    网络库封装

--- oo-server/src/main/java/com/lsm1998/oo/ServerApplication.java
+++ oo-server/src/main/java/com/lsm1998/oo/ServerApplication.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class ServerApplication
 {
+    // https://www.cnblogs.com/f1194361820/p/4019575.html
     public static void main(String[] args) throws IOException
     {
         NetServer netServer = new NetServerBuild().
